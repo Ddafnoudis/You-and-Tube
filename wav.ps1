9acc74f4a6848bf08440321cd51b6fa17e41679c
Dimitris Dafnoudis <dimitrisdaf@hotmail.com>
2024-10-09 12:10:39 +0200

    Powershell script for windows users

---
+++ wav.ps1
@@ -0,0 +1,13 @@
+Write-Host "Hello Elisavet! Time to download some WAV files!"
+
+Write-Host "" 
+
+# Prompt the user for a YouTube URL
+$url = Read-Host "Enter the YouTube URL"
+
+# Run yt-dlp to download and convert to WAV format
+yt-dlp "$url" -x --audio-format wav -o ".\win_wav_files\%(title)s.%(ext)s" --ffmpeg-location ".\ffmpeg-master-latest-win64-gpl\bin\"
+
+Write-Host ""
+
+Write-Host "The download has finished! Enjoy the Good Music!"
